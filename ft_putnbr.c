2a05db9a3bbbb93dfbac5159defbf40d7ba3602d
lvallie <lvallie@student.21-school.ru>
2021-05-30 06:56:44 +0300

    change folder

--- ft_putnbr.c
+++
@@ -1,9 +0,0 @@
-#include "minitalk.h"
-
-void	ft_putnbr(int number)
-{
-	if (number > 9)
-		ft_putnbr(number / 10);
-	number = (number % 10) + 48;
-	write(1, &number, 1);
-}
--- server.c
+++
@@ -1,8 +0,0 @@
-#include "minitalk.h"
-#include <stdio.h>
-
-int	main(void)
-{
-	ft_printpid();
-	pause();
-}
